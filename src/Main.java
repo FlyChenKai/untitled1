fb586cbe91116cb380040e246d67d871187f6cb3
FlyChenKai <1628343148@qq.com>
2018-10-19 21:09:54 +0800

    set,map的相关操作

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
         Map<String,String> map=new HashMap<>(5);
         for(int i=0;i<5;i++)
             map.put(String.valueOf(i),String.valueOf(i*i));
-        for(Map.Entry<String,String> e:map.entrySet()){
+
+        for(Map.Entry<String,String> e:map.entrySet()){//map的遍历
             System.out.println(e.getKey()+" "+e.getValue());
         }
         System.out.println(map.get("1"));
@@ -18,12 +19,14 @@ public class Main {
         for(int i=0;i<7;i++){
             set.add(String.valueOf(i));
         }
-        set.add("1");
+        set.add("1");//集合无重复元素
+
+
         List<Integer> list=new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        Collections.shuffle(list);
+        Collections.shuffle(list);//随机排列
        for(int i:list)
            System.out.print(i);
 
